d7cf67713fc6950495a89669e8224c9802fb9f2d
fahrilar <96267756+FAHMEN@users.noreply.github.com>
2023-04-18 20:03:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
-global.owner = [
-  ['6281390658325'],
-  ['6282256115584'],
-  ['6281319944687', 'BG', true]
+global.owner = [  
+  ['6285876902820'],
+  ['6285876902820'],
+  ['6285876902820', 'BG', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -30,8 +30,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot'
+global.packname = 'wa.me/79269175361'
+global.author = 'fahrilbotz'
 
 global.multiplier = 69 // The higher, The harder levelup
 
